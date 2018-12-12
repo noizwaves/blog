6bb8d95df2c4156927b068ed73c8f27a1b5f5fde
Adam Neumann <noizwaves@users.noreply.github.com>
2018-12-11 20:55:15 -0700

    Group related code together

--- Program.fs
+++ Program.fs
@@ -14,11 +14,20 @@ open System.IO
 open System.Text
 open FSharp.Markdown
 
-type PostHtmlDto =
-    { title : String
-      createdAt : String
-      bodyHtml : String }
+// Domain
+type CreateDate = CreateDate of System.DateTime
 
+let parseFromLongString (value : String) : CreateDate option =
+    let date = System.DateTime.Parse(value)
+    Some <| CreateDate date
+
+type BlogPost =
+    { slug : String
+      title : String
+      createdAt : CreateDate
+      body : String }
+
+// Disk serialisation
 let private shallowYamlDecode (yml : String) : Map<String, String> =
     yml
     |> String.split '\n'
@@ -41,62 +50,6 @@ let private parsePostFile (raw : String) =
         (frontmatter, document)
     else (Map.empty, raw)
 
-let rec private viewSpan (s : MarkdownSpan) =
-    match s with
-    | Literal(text) -> text
-    | InlineCode(code) -> sprintf "<code class=\"highlighter-rouge\">%s</code>" code
-    | Strong(span) -> sprintf "<strong>%s</strong>" (viewSpans span)
-    | Emphasis _ -> "$$Emphasis"
-    | AnchorLink _ -> "$$AnchorLink"
-    | DirectLink(body, (link, _)) -> sprintf """<a href="%s">%s</a>""" link (viewSpans body)
-    | IndirectLink _ -> "$$IndirectLink"
-    | DirectImage(body, (link, _)) -> 
-        let src = link |> String.replace "{{ site.url }}" ""
-        sprintf "<img src=\"%s\" alt=\"%s\" />" src body
-    | IndirectImage _ -> "$$IndirectImage"
-    | HardLineBreak _ -> "<br>"
-    | LatexInlineMath _ -> "$$LatexInlineMath"
-    | LatexDisplayMath _ -> "$$LatexDisplayMath"
-    | EmbedSpans _ -> "$$EmbedSpans"
-
-and private viewSpans (spans : MarkdownSpans) =
-    spans
-    |> List.map viewSpan
-    |> List.reduce (fun s1 s2 -> s1 + s2)
-
-let private viewParagraph p =
-    match p with
-    | Heading(size, spans) -> sprintf "<h%i>%s</h%i>" size (viewSpans spans) size
-    | Paragraph(spans) -> sprintf "<p>%s</p>" (viewSpans spans)
-    | CodeBlock _ -> "$$CodeBlock"
-    | InlineBlock _ -> "$$InlineBlock"
-    | ListBlock _ -> "$$ListBlock"
-    | QuotedBlock _ -> "$$QuotedBlock"
-    | Span _ -> "$$Span"
-    | LatexBlock _ -> "$$LatexBlock"
-    | HorizontalRule _ -> "$$HorizontalRule"
-    | TableBlock _ -> "$$TableBlock"
-    | EmbedParagraphs _ -> "$$EmbedParagraphs"
-
-let private toHtmlString (document : MarkdownDocument) : String =
-    document.Paragraphs
-    |> List.map viewParagraph
-    |> List.reduce (fun s1 s2 -> s1 + s2)
-
-type CreateDate = CreateDate of System.DateTime
-
-let parseFromLongString (value : String) : CreateDate option =
-    let date = System.DateTime.Parse(value)
-    Some <| CreateDate date
-
-let formatCreateDate (CreateDate value) : String = value.ToString("MMM d, yyyy")
-
-type BlogPost =
-    { slug : String
-      title : String
-      createdAt : CreateDate
-      body : String }
-
 let private fromRawString (slug : String) (raw : String) : BlogPost =
     let (frontMatter, body) = parsePostFile raw
     let title = Map.tryFind "title" frontMatter |> Option.defaultValue (String.replace "-" " " slug)
@@ -117,21 +70,77 @@ let private fromRawString (slug : String) (raw : String) : BlogPost =
       createdAt = createdAt
       body = body }
 
-let private handleBlogPost slug =
-    request (fun r -> 
-        let post =
-            sprintf "_posts/%s.md" slug
-            |> System.IO.File.ReadAllText
-            |> fromRawString slug
-        
-        let parsed = Markdown.Parse(post.body)
-        let htmlBody = toHtmlString parsed
-        
-        let model =
-            { title = post.title
-              createdAt = post.createdAt |> formatCreateDate
-              bodyHtml = htmlBody }
-        page "post.html.liquid" model)
+// HTML, Markdown formatting
+let rec private viewSpan (s : MarkdownSpan) =
+    match s with
+    | Literal(text) -> text
+    | InlineCode(code) -> sprintf "<code class=\"highlighter-rouge\">%s</code>" code
+    | Strong(span) -> sprintf "<strong>%s</strong>" (viewSpans span)
+    | Emphasis _ -> failwith "Emphasis not translated yet"
+    | AnchorLink _ -> failwith "AnchorLink not translated yet"
+    | DirectLink(body, (link, _)) -> sprintf """<a href="%s">%s</a>""" link (viewSpans body)
+    | IndirectLink _ -> failwith "IndirectLink not translated yet"
+    | DirectImage(body, (link, _)) -> 
+        let src = link |> String.replace "{{ site.url }}" ""
+        sprintf "<img src=\"%s\" alt=\"%s\" />" src body
+    | IndirectImage _ -> failwith "IndirectImage not translated yet"
+    | HardLineBreak _ -> "<br>"
+    | LatexInlineMath _ -> failwith "LatexInlineMath not translated yet"
+    | LatexDisplayMath _ -> failwith "LatexDisplayMath not translated yet"
+    | EmbedSpans _ -> failwith "EmbedSpans not translated yet"
+
+and private viewSpans (spans : MarkdownSpans) =
+    spans
+    |> List.map viewSpan
+    |> List.reduce (fun s1 s2 -> s1 + s2)
+
+let private viewParagraph p =
+    match p with
+    | Heading(size, spans) -> sprintf "<h%i>%s</h%i>" size (viewSpans spans) size
+    | Paragraph(spans) -> sprintf "<p>%s</p>" (viewSpans spans)
+    | CodeBlock _ -> failwith "CodeBlock not translated yet"
+    | InlineBlock _ -> failwith "InlineBlock not translated yet"
+    | ListBlock _ -> failwith "ListBlock not translated yet"
+    | QuotedBlock _ -> failwith "QuotedBlock not translated yet"
+    | Span _ -> failwith "Span not translated yet"
+    | LatexBlock _ -> failwith "LatexBlock not translated yet"
+    | HorizontalRule _ -> failwith "HorizontalRule not translated yet"
+    | TableBlock _ -> failwith "TableBlock not translated yet"
+    | EmbedParagraphs _ -> failwith "EmbedParagraphs not translated yet"
+
+let private toHtmlString (document : MarkdownDocument) : String =
+    document.Paragraphs
+    |> List.map viewParagraph
+    |> List.reduce (fun s1 s2 -> s1 + s2)
+
+// HTML
+type PostHtmlDto =
+    { title : String
+      createdAt : String
+      bodyHtml : String }
+
+type PostItemHtmlDto =
+    { title : String
+      createdAt : String
+      link : String }
+
+type PostsHtmlDto =
+    { posts : PostItemHtmlDto list }
+
+let formatCreateDate (CreateDate value) : String = value.ToString("MMM d, yyyy")
+
+let private derivePostUrl (post : BlogPost) : String =
+    match String.split '-' post.slug with
+    | (year :: month :: day :: rest) -> sprintf "/%s/%s/%s/%s" year month day (String.concat "-" rest)
+    | _ -> failwith "Invalid filename"
+
+// Flows
+let private safeFind predicate list =
+    try 
+        list
+        |> List.find predicate
+        |> Some
+    with :? System.Collections.Generic.KeyNotFoundException -> None
 
 let private handleBlogPostPrecise (year, month, day, titleSlug) =
     request (fun r -> 
@@ -145,17 +154,9 @@ let private handleBlogPostPrecise (year, month, day, titleSlug) =
                    |> fromRawString slug)
         
         let slug = sprintf "%04i-%02i-%02i-%s" year month day titleSlug
-        
-        let safeFind predicate list =
-            try 
-                list
-                |> List.find predicate
-                |> Some
-            with :? System.Collections.Generic.KeyNotFoundException -> None
-        
         let post = allPosts |> safeFind (fun p -> p.slug.Equals(slug))
         
-        let toDto post : PostHtmlDto =
+        let toDto (post : BlogPost) : PostHtmlDto =
             { title = post.title
               createdAt = post.createdAt |> formatCreateDate
               bodyHtml =
@@ -167,19 +168,6 @@ let private handleBlogPostPrecise (year, month, day, titleSlug) =
         match dto with
         | Some dto -> page "post.html.liquid" dto
         | None -> NOT_FOUND "404")
-
-type PostItemHtmlDto =
-    { title : String
-      createdAt : String
-      link : String }
-
-type PostsHtmlDto =
-    { posts : PostItemHtmlDto list }
-
-let private derivePostUrl (post : BlogPost) : String =
-    match String.split '-' post.slug with
-    | (year :: month :: day :: rest) -> sprintf "/%s/%s/%s/%s" year month day (String.concat "-" rest)
-    | _ -> failwith "Invalid filename"
 
 let private handleBlogPosts request =
     let posts =
