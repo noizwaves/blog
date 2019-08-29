60d4fae59f06ea7a42adbcd0f7135cb6fcf0981d
Adam Neumann <noizwaves@users.noreply.github.com>
2019-08-29 16:57:43 -0400

    Fix test formatting

--- NoizwavesBlog.Tests/MarkdownTests.fs
+++ NoizwavesBlog.Tests/MarkdownTests.fs
@@ -5,149 +5,149 @@ open NoizwavesBlog.OwnMarkdown
 
 
 let private debug =
-    if not(System.Diagnostics.Debugger.IsAttached) then
+    if not (System.Diagnostics.Debugger.IsAttached) then
       printfn "Please attach a debugger, PID: %d" (System.Diagnostics.Process.GetCurrentProcess().Id)
-    while not(System.Diagnostics.Debugger.IsAttached) do
+    while not (System.Diagnostics.Debugger.IsAttached) do
       System.Threading.Thread.Sleep(100)
 
 [<Fact>]
-let ``Single line paragraph`` () =
-    let expected : Markdown = [ Paragraph [ Span "Foo" ] ]
-    let actual : Markdown = ParseOwn """Foo""" |> Option.get
-    Assert.Equal<Markdown> (expected, actual)
+let ``Single line paragraph``() =
+    let expected: Markdown = [ Paragraph [ Span "Foo" ] ]
+    let actual: Markdown = ParseOwn """Foo""" |> Option.get
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Single line paragraph with newline`` () =
-    let expected : Markdown = [ Paragraph [ Span "Foo" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """Foo
+let ``Single line paragraph with newline``() =
+    let expected: Markdown = [ Paragraph [ Span "Foo" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """Foo
 """
-    Assert.Equal<Markdown> (expected, actual)
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Multiple single line paragraphs`` () =
-    let expected : Markdown = [ Paragraph [ Span "Foo" ]; Paragraph [ Span "Bar" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """Foo
+let ``Multiple single line paragraphs``() =
+    let expected: Markdown = [ Paragraph [ Span "Foo" ]; Paragraph [ Span "Bar" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """Foo
 
 Bar"""
-    Assert.Equal<Markdown> (expected, actual)
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Multiple line paragraph`` () =
-    let expected : Markdown =
+let ``Multiple line paragraph``() =
+    let expected: Markdown =
         [ Paragraph [ Span "Foo"; Span "Bar" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """Foo
+    let actual: Markdown = Option.get <| ParseOwn """Foo
 Bar"""
-    Assert.Equal<Markdown> (expected, actual)
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Multiple paragraphs`` () =
-    let expected : Markdown =
+let ``Multiple paragraphs``() =
+    let expected: Markdown =
         [ Paragraph [ Span "Foo" ]
-        ; Paragraph [ Span "Bar"; Span "Baz" ]
+       ; Paragraph [ Span "Bar"; Span "Baz" ]
         ]
-    let actual : Markdown = Option.get <| ParseOwn """Foo
+    let actual: Markdown = Option.get <| ParseOwn """Foo
 
 Bar
 Baz"""
-    Assert.Equal<Markdown> (expected, actual)
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Emphasized text in a paragraph`` () =
-    let expected : Markdown =
+let ``Emphasized text in a paragraph``() =
+    let expected: Markdown =
         [ Paragraph [ Emphasized "Foo" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """_Foo_"""
-    Assert.Equal<Markdown> (expected, actual)
+    let actual: Markdown = Option.get <| ParseOwn """_Foo_"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Bold text in a paragraph`` () =
-    let expected : Markdown =
+let ``Bold text in a paragraph``() =
+    let expected: Markdown =
         [ Paragraph [ Bolded "Foo" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """**Foo**"""
-    Assert.Equal<Markdown> (expected, actual)
+    let actual: Markdown = Option.get <| ParseOwn """**Foo**"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Alternatively bolded text in a paragraph`` () =
-    let expected : Markdown =
+let ``Alternatively bolded text in a paragraph``() =
+    let expected: Markdown =
         [ Paragraph [ Bolded "Foo" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """__Foo__"""
-    Assert.Equal<Markdown> (expected, actual)
+    let actual: Markdown = Option.get <| ParseOwn """__Foo__"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Mix of regular, bolded, and emphasized text in a paragraph`` () =
-    let expected : Markdown =
+let ``Mix of regular, bolded, and emphasized text in a paragraph``() =
+    let expected: Markdown =
         [ Paragraph
             [ Emphasized "Foo"
-            ; Span " Bar"
-            ; Span "Baz "
-            ; Emphasized "Qux"
-            ; Bolded "Quux"
-            ; Span " Quuz "
-            ; Emphasized "Corge"
-            ; Span " "
-            ; Bolded "Grault"
+           ; Span " Bar"
+           ; Span "Baz "
+           ; Emphasized "Qux"
+           ; Bolded "Quux"
+           ; Span " Quuz "
+           ; Emphasized "Corge"
+           ; Span " "
+           ; Bolded "Grault"
             ]
         ]
-    let actual : Markdown = Option.get <| ParseOwn """_Foo_ Bar
+    let actual: Markdown = Option.get <| ParseOwn """_Foo_ Bar
 Baz _Qux_
 **Quux** Quuz _Corge_ __Grault__"""
-    Assert.Equal<Markdown> (expected, actual)
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Inline link in a paragraph`` () =
-    let expected : Markdown = [ Paragraph [ InlineLink ("www.example.com", "foobar") ] ]
-    let actual : Markdown = Option.get <| ParseOwn """[foobar](www.example.com)"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Inline link in a paragraph``() =
+    let expected: Markdown = [ Paragraph [ InlineLink("www.example.com", "foobar") ] ]
+    let actual: Markdown = Option.get <| ParseOwn """[foobar](www.example.com)"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Inline code span using single backticks`` () =
-    let expected : Markdown = [ Paragraph [ Code "foobar" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """`foobar`"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Inline code span using single backticks``() =
+    let expected: Markdown = [ Paragraph [ Code "foobar" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """`foobar`"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Inline code span using double backticks`` () =
-    let expected : Markdown = [ Paragraph [ Code "baz" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """``baz``"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Inline code span using double backticks``() =
+    let expected: Markdown = [ Paragraph [ Code "baz" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """``baz``"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Inline code span containing a literal backtick`` () =
-    let expected : Markdown = [ Paragraph [ Code "b`az" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """``b`az``"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Inline code span containing a literal backtick``() =
+    let expected: Markdown = [ Paragraph [ Code "b`az" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """``b`az``"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Inline code span containing two literal backticks`` () =
-    let expected : Markdown = [ Paragraph [ Code "b`a`z" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """``b`a`z``"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Inline code span containing two literal backticks``() =
+    let expected: Markdown = [ Paragraph [ Code "b`a`z" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """``b`a`z``"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Inline code span containing multiple literal backticks`` () =
-    let expected : Markdown = [ Paragraph [ Code "b`a`z` `q`u`x" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """``b`a`z` `q`u`x``"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Inline code span containing multiple literal backticks``() =
+    let expected: Markdown = [ Paragraph [ Code "b`a`z` `q`u`x" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """``b`a`z` `q`u`x``"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Inline code span containing multiple token characters`` () =
-    let expected : Markdown = [ Paragraph [ Code "([_*])" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """`([_*])`"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Inline code span containing multiple token characters``() =
+    let expected: Markdown = [ Paragraph [ Code "([_*])" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """`([_*])`"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Inline code span containing a syntactically valid link`` () =
-    let expected : Markdown = [ Paragraph [ Code "[not a link](www.example.com)" ]]
-    let actual : Markdown = Option.get <| ParseOwn """`[not a link](www.example.com)`"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Inline code span containing a syntactically valid link``() =
+    let expected: Markdown = [ Paragraph [ Code "[not a link](www.example.com)" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """`[not a link](www.example.com)`"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Double backticked inline code span containing multiple token characters`` () =
-    let expected : Markdown = [ Paragraph [ Code "([_*])" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """``([_*])``"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Double backticked inline code span containing multiple token characters``() =
+    let expected: Markdown = [ Paragraph [ Code "([_*])" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """``([_*])``"""
+    Assert.Equal<Markdown>(expected, actual)
 
 [<Fact>]
-let ``Double backticked inline code span containing multiple token characters and escaped backticks`` () =
-    let expected : Markdown = [ Paragraph [ Code "([_`*`])" ] ]
-    let actual : Markdown = Option.get <| ParseOwn """``([_`*`])``"""
-    Assert.Equal<Markdown> (expected, actual)
+let ``Double backticked inline code span containing multiple token characters and escaped backticks``() =
+    let expected: Markdown = [ Paragraph [ Code "([_`*`])" ] ]
+    let actual: Markdown = Option.get <| ParseOwn """``([_`*`])``"""
+    Assert.Equal<Markdown>(expected, actual)
