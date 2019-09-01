e5ecd4ee8a3bc958ca0bf1f68964a44bd8885406
Adam Neumann <noizwaves@users.noreply.github.com>
2019-09-01 10:32:25 -0600

    Use own markdown parser instead of Fsharp Formatting

--- NoizwavesBlog/Markdown.fs
+++ NoizwavesBlog/Markdown.fs
@@ -1,69 +1,70 @@
 module NoizwavesBlog.Markdown
 
-open System
-open FSharp.Markdown
+open NoizwavesBlog.OwnMarkdown
 
-let private joinStrings = List.reduce (fun s1 s2 -> s1 + s2)
+let viewElement (elements: MarkdownElement): string =
+    match elements with
+    | Span(text) -> text
+    | Emphasized(text) -> sprintf "<em>%s</em>" text
+    | Bolded(text) -> sprintf "<strong>%s</strong>" text
+    | InlineLink(url, text) -> sprintf """<a href="%s">%s</a>""" url text
+    | Code(code) -> sprintf "<code class=\"highlighter-rouge\">%s</code>" code
+    | Image(url, alt) ->
+        let src = url |> String.replace "{{ site.url }}" ""
+        sprintf "<img src=\"%s\" alt=\"%s\" />" src alt
 
-let rec private viewSpan (s : MarkdownSpan) =
-    match s with
-    | Literal(text) -> text
-    | InlineCode(code) -> sprintf "<code class=\"highlighter-rouge\">%s</code>" code
-    | Strong(span) -> sprintf "<strong>%s</strong>" (viewSpans span)
-    | Emphasis(spans) -> sprintf "<em>%s</em>" (viewSpans spans)
-    | AnchorLink _ -> failwith "AnchorLink not translated yet"
-    | DirectLink(body, (link, _)) -> sprintf """<a href="%s">%s</a>""" link (viewSpans body)
-    | IndirectLink _ -> failwith "IndirectLink not translated yet"
-    | DirectImage(body, (link, _)) ->
-        let src = link |> String.replace "{{ site.url }}" ""
-        sprintf "<img src=\"%s\" alt=\"%s\" />" src body
-    | IndirectImage _ -> failwith "IndirectImage not translated yet"
-    | HardLineBreak _ -> "<br>"
-    | LatexInlineMath _ -> failwith "LatexInlineMath not translated yet"
-    | LatexDisplayMath _ -> failwith "LatexDisplayMath not translated yet"
-    | EmbedSpans _ -> failwith "EmbedSpans not translated yet"
+let viewParagraph (paragraph: MarkdownParagraph): string =
+    let viewElements (elements: MarkdownElement list): string =
+        elements
+        |> List.map viewElement
+        |> String.concat ""
 
-and private viewSpans (spans : MarkdownSpans) : string =
-    spans
-    |> List.map viewSpan
-    |> joinStrings
+    let viewListItem (ListItem elements): string =
+        viewElements elements
 
-let rec private viewParagraph p =
-    match p with
-    | Heading(size, spans) -> sprintf "<h%i>%s</h%i>" size (viewSpans spans) size
-    | Paragraph(spans) -> sprintf "<p>%s</p>" (viewSpans spans)
-    | CodeBlock(code, _, _) ->
-        sprintf
-            """<div class="highlighter-rouge"><div class="highlight"><pre class="highlight"><code>%s</code></pre></div></div>"""
-            code
-    | InlineBlock _ -> failwith "InlineBlock not translated yet"
-    | ListBlock(kind, items) ->
-            items
-            |> List.map viewParagraphs
-            |> List.map (sprintf "<li>%s</li>")
-            |> joinStrings
-            |> sprintf "<ul>%s</ul>"
-    | QuotedBlock(paragraphs) ->
-            paragraphs
-            |> viewParagraphs
-            |> sprintf "<blockquote>%s</blockquote>"
-    | Span(spans) -> spans |> viewSpans |> sprintf "<span>%s</span>"
-    | LatexBlock _ -> failwith "LatexBlock not translated yet"
-    | HorizontalRule _ -> failwith "HorizontalRule not translated yet"
-    | TableBlock _ -> failwith "TableBlock not translated yet"
-    | EmbedParagraphs _ -> failwith "EmbedParagraphs not translated yet"
+    match paragraph with
+    | Heading1(elements) ->
+        elements
+        |> viewElements
+        |> sprintf "<h1>%s</h1>"
+    | Heading2(elements) ->
+        elements
+        |> viewElements
+        |> sprintf "<h2>%s</h2>"
+    | Heading3(elements) ->
+        elements
+        |> viewElements
+        |> sprintf "<h3>%s</h3>"
+    | Paragraph(lines) ->
+        lines
+        |> List.map viewElements
+        |> String.concat "\n"
+        |> sprintf "<p>%s</p>"
+    | CodeBlock(code, _) ->
+        sprintf """<div class="highlighter-rouge"><div class="highlight"><pre class="highlight"><code>%s</code></pre></div></div>""" code
+    | QuoteBlock(text) ->
+        sprintf "<blockquote>%s</blockquote>" text
+    | OrderedList(items) ->
+        sprintf "%O" items |> ignore
+        items
+        |> List.map viewListItem
+        |> List.map (sprintf "<li>%s</li>")
+        |> String.concat ""
+        |> sprintf "<ol>%s</ol>"
+    | UnorderedList(items) ->
+        items
+        |> List.map viewListItem
+        |> List.map (sprintf "<li>%s</li>")
+        |> String.concat ""
+        |> sprintf "<ul>%s</ul>"
 
-and private viewParagraphs (paragraphs : MarkdownParagraphs) : string =
-    paragraphs
-    |> List.map viewParagraph
-    |> joinStrings
-
-let private toHtmlString (document : MarkdownDocument) : string =
-    document.Paragraphs
-    |> List.map viewParagraph
-    |> joinStrings
-
-let convertToHtml (markdown : String) : String =
+let toHtmlString (markdown: Markdown): string =
     markdown
-    |> Markdown.Parse
+    |> List.map viewParagraph
+    |> String.concat ""
+
+let convertToHtml (markdown: string) : string =
+    markdown
+    |> ParseOwn
+    |> Option.get
     |> toHtmlString
